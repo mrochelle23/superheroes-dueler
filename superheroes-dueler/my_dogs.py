2bf036ebf2983abbe3f4c2c13aab37eb7521910e
mrochelle23 <142530319+mrochelle23@users.noreply.github.com>
2023-10-30 14:19:51 -0500

    Add files via upload

---
+++ superheroes-dueler/my_dogs.py
@@ -0,0 +1,10 @@
+# my_dogs.py
+import dog
+...
+
+my_other_dog = dog.Dog("Annie", "SuperDog")
+my_other_dog2 = dog.Dog("Jefflepuff", "Labrador Pitbull")
+my_other_dog3 = dog.Dog("Ommision", "Orange Retriever")
+
+dogs = [my_other_dog, my_other_dog2, ]
+print(my_other_dog.name)
